4b57b011d723d77c7918d95c262cddeb0fd1d619
Harshavardhan Nalajala <hnalajala@nvidia.com>
2014-04-02 15:08:51 +0530

    check connection state before connecting
    when two headsets are paired and connection to
    the headsets is toggled rapidly, connectToOtherProfile
    tries to connect device without checking the current
    state of profile on one of the bonded devices.
    If another device is in connecting state for the same
    profile, once it gets connected, connect message arrives
    and disconnects the device. This causes one device
    to connect in hfp and another device to connect in a2dp.
    
    Bug 68798
    
    Change-Id: I568ba5e070414515497e9101750b99c9ac09e3ae

--- src/com/android/bluetooth/hfp/HeadsetService.java
+++ src/com/android/bluetooth/hfp/HeadsetService.java
@@ -355,7 +355,7 @@ public class HeadsetService extends ProfileService {
         return mStateMachine.getDevicesMatchingConnectionStates(states);
     }
 
-    int getConnectionState(BluetoothDevice device) {
+    public int getConnectionState(BluetoothDevice device) {
         enforceCallingOrSelfPermission(BLUETOOTH_PERM, "Need BLUETOOTH permission");
         return mStateMachine.getConnectionState(device);
     }
